a6bfdbfb817b969355d4835320a352abc9f9cfdf
jesse.chung9727@gmail.com <jesse.chung9727@gmail.com>
2023-08-15 20:02:38 -0400

    first code review

--- routes/shipments.js
+++ routes/shipments.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const { BadRequestError } = require("../expressError");
 const router = new express.Router();
+const jsonschema = require("jsonschema");
+const shippingSchema = require("../schemas/shippingSchema.json");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -18,6 +20,14 @@ router.post("/", async function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
   }
+
+  const result = jsonschema.validate(
+    req.body, shippingSchema, {required: true});
+  if (!result.valid){
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -16,10 +16,179 @@ describe("POST /", function () {
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
 
-  test("throws error if empty request body", async function () {
+
+  test("throws error if no request body", async function () {
     const resp = await request(app)
       .post("/shipments")
       .send();
     expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": "Bad Request",
+        "status": 400
+      }
+    });
   });
+
+
+  test("throws error if empty body", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({});
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance requires property \"productId\"",
+          "instance requires property \"name\"",
+          "instance requires property \"addr\"",
+          "instance requires property \"zip\""
+        ],
+        "status": 400
+      }
+    });
+  });
+
+
+
+  test("invalid error messages", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": 899,
+        "name": 10,
+        "zip": "1234789000000000"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId must be greater than or equal to 1000",
+          "instance.name is not of a type(s) string",
+          "instance.zip does not meet maximum length of 10",
+          "instance requires property \"addr\""
+        ],
+        "status": 400
+      }
+    });
+  });
+
+
+  test("wrong input types", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": "bob",
+        "name": 10,
+        "zip": 10000000,
+        "addr": 100
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId is not of a type(s) integer",
+          "instance.name is not of a type(s) string",
+          "instance.addr is not of a type(s) string",
+          "instance.zip is not of a type(s) string"
+        ],
+        "status": 400
+      }
+    });
+  });
+  //TODO: test no product Id, no product name, no zipcode, no address
+  //TODO: test if product Id is under 1000, should be invalid
+  //TODO: test if zipcode is too short or too long (two separate tests)
+  //TODO: test if we sent the wrong types
+
+  test("invalid zip code, too long", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": 1000,
+        "name": "Test Tester",
+        "addr": "100 Test St",
+        "zip": "12345-6789123213213123"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.zip does not meet maximum length of 10"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+
+  test("invalid zip code, too short", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": 1000,
+        "name": "Test Tester",
+        "addr": "100 Test St",
+        "zip": "12"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+        "error": {
+          "message": [
+            "instance.zip does not meet minimum length of 5"
+          ],
+          "status": 400
+        }
+      });
+  });
+
+
+
+  test("item Id has to be at least 1000", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": 999,
+        "name": "Test Tester",
+        "addr": "100 Test St",
+        "zip": "12345-6789"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId must be greater than or equal to 1000"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+
+  test("no additional properties are sent", async function () {
+    const resp = await request(app)
+      .post('/shipments')
+      .send({
+        "productId": 1000,
+        "name": "Test Tester",
+        "addr": "100 Test St",
+        "zip": "12345-6789",
+        "phone": 1234567
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance is not allowed to have the additional property \"phone\""
+        ],
+        "status": 400
+      }
+    });
+  });
+
+
+
+
+
+
 });
